e6c7e327c8cc58cfb01db119c09c2efdd946322a
Svetlana Sultanyan <svetlannasultanyan@gmail.com>
2026-06-12 14:54:38 +0200

    Merge conflickt fixed

--- src/server.js
+++ src/server.js
@@ -3,21 +3,21 @@ const app = express();
 
 app.use(express.json());
 
-
 const authRoutes = require('../src/routes/auth');
 const patientsRoutes = require('./routes/patientsRouteur');
 
-const medecinsRoutess = require('./routes/medecinsRouteur');
 
+const medecinsRoutess = require('./routes/medecinsRouteur');
 
 const appointmentsRoutes = require('./routes/appointments');
 
 app.use('/auth', authRoutes);
 
 app.use('/patients', patientsRoutes);
-
  app.use('/medecins', medecinsRoutess);
 app.use('/appointments', appointmentsRoutes);
 
 app.listen(3000, () => console.log("Serveur prêt sur http://localhost:3000"));
+
+
 
