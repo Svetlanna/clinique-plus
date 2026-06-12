28df70095cc2f07da48b4b11fbf160017759ec7a
Svetlana Sultanyan <svetlannasultanyan@gmail.com>
2026-06-12 12:18:17 +0200

    Cleaning code de users model/controller/authificatiin routeur

--- Model/userModel.js
+++ Model/userModel.js
@@ -1,13 +1,21 @@
-const db = require('../src/database');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+const dbPath = path.resolve(__dirname, '../data/database.sqlite');
+const db = new sqlite3.Database(dbPath);
+
+module.exports = db;
 
 const findUserByMail = (mail) => {
     return new Promise((resolve, reject) => {
 
-        const sql = `SELECT id, mail, password, role FROM users WHERE mail = ?`;
+        const sql = `SELECT * FROM users WHERE mail = ?`;
 
-        db.get(sql, [mail], (err, user) => {
-            if (err) reject(err);
-            else resolve(user);
+db.get(sql, [mail], (err, user) => {
+    if (err) {
+                reject(err);
+            } else {
+                resolve(user);
+            }
         });
     });
 };
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const app = express();
 const router = express.Router();
 
 const userController = require('../../Controller/UserController');
+
 router.post('/login', userController.login);
 
 module.exports = router;
